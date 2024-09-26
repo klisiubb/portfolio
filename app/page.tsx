69f7a7a7be58306cf3f9c879f24c0ef62b0d946a
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-09-26 16:58:16 +0200

    Setup Magic UI and Logo Aceternity UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import TypingAnimation from "@/components/magicui/typing-animation";
+
 export default function Page() {
-  return <></>;
+  return (
+    <div className="h-screen flex flex-row justify-center items-center">
+      <TypingAnimation
+        text="Hi, I'm Mateusz Kliś"
+        duration={50}
+        className="text-6xl"
+      />
+    </div>
+  );
 }
