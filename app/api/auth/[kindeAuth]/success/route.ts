00b5ea29d2eaf02d2a719f22367fefc257e7faaa
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-09-30 13:19:13 +0200

    Auth setup

---
+++ app/api/auth/[kindeAuth]/success/route.ts
@@ -0,0 +1,32 @@
+import { prisma } from "@/lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
+export async function GET() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  const website = process.env.WEBSITE_URL || "http://localhost:3000/";
+
+  if (!user || user == null || !user.id)
+    throw new Error("something went wrong with authentication" + user);
+
+  let dbUser = await prisma.user.findUnique({
+    where: { id: user.id },
+  });
+
+  if (!dbUser) {
+    dbUser = await prisma.user.create({
+      data: {
+        id: user.id,
+        firstName: user.given_name ?? "",
+        lastName: user.family_name ?? "",
+        email: user.email ?? "",
+      },
+    });
+  }
+
+  return NextResponse.redirect(website);
+}
